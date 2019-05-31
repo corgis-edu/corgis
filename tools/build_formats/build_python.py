71a84c95c74d81ab278fbbd2fc120f42b8c9653d
Austin Cory Bart <acbart@vt.edu>
2019-05-31 19:35:48 -0400

    Good progress on loading in a dataset

--- tools/build_formats/build_python.py
+++ tools/build_formats/build_python.py
@@ -1,5 +1,11 @@
 import csv
+import time
+
+from tools.build_report import BuildReport
 
 def build(dataset):
+    start = time.time()
     print(dataset)
-    return "SUCCESS"
+    end_time = time.time()-start
+    report = BuildReport(dataset, 'python', end_time, [])
+    return report
