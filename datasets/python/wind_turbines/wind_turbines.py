893bc6852ea64543790c47a43eac914b082ef715
Austin Cory Bart <acbart@vt.edu>
2019-06-08 14:40:44 -0400

    Wind turbine dataset (python)

---
+++ datasets/python/wind_turbines/wind_turbines.py
@@ -0,0 +1,116 @@
+'''
+Hello! Thank you for downloading a CORGIS library. However, you do not
+need to open this file. Instead you should make your own Python file and
+add the following line:
+
+import wind_turbines
+
+Then just place the files you downloaded alongside it.
+'''
+
+import os as _os
+import pickle as _pickle
+
+__all__ = ['get_turbines']
+
+def _tifa_definitions():
+    return {"type": "ModuleType",
+        "fields": {
+            'get': {
+                "type": "FunctionType",
+                "name": 'get',
+                "returns": {
+                    "type": "ListType", 
+                    "empty": False, 
+                    "subtype": {"type": "NumType"}
+                }
+            },
+            'get_turbines': {
+                "type": "FunctionType",
+                "name": 'get_turbines',
+                "returns": {
+                    "type": "ListType", 
+                    "empty": False, 
+                    "subtype": {
+                        "type": "DictType",
+                        "literals": [
+                            {"type": "LiteralStr", "value": "Site.State"},
+                            {"type": "LiteralStr", "value": "Site.County"},
+                            {"type": "LiteralStr", "value": "Year"},
+                            {"type": "LiteralStr", "value": "Turbine.Capacity"},
+                            {"type": "LiteralStr", "value": "Turbine.Hub_Height"},
+                            {"type": "LiteralStr", "value": "Turbine.Rotor_Diameter"},
+                            {"type": "LiteralStr", "value": "Turbine.Swept_Area"},
+                            {"type": "LiteralStr", "value": "Turbine.Total_Height"},
+                            {"type": "LiteralStr", "value": "Project.Capacity"},
+                            {"type": "LiteralStr", "value": "Project.Number_Turbines"},
+                            {"type": "LiteralStr", "value": "Site.Latitude"},
+                            {"type": "LiteralStr", "value": "Site.Longitude"},
+                        ],
+                        "values": [
+                            {"type": "StrType"},
+                            {"type": "StrType"},
+                            {"type": "NumType"},
+                            {"type": "NumType"},
+                            {"type": "NumType"},
+                            {"type": "NumType"},
+                            {"type": "NumType"},
+                            {"type": "NumType"},
+                            {"type": "NumType"},
+                            {"type": "NumType"},
+                            {"type": "NumType"},
+                            {"type": "NumType"},
+                        ]
+                    }
+                }
+            },
+        }
+    }
+
+class _Constants(object):
+    '''
+    Global singleton object to hide some of the constants; some IDEs reveal
+    internal module details very aggressively, and there's no other way
+    to hide stuff.
+    '''
+
+class DatasetException(Exception):
+    ''' Thrown when there is an error loading the dataset for some reason.'''
+    
+_Constants._DATABASE_NAME = _os.path.join(_os.path.dirname(__file__),
+                                          "wind_turbines.data")
+if not _os.access(_Constants._DATABASE_NAME, _os.F_OK):
+    raise DatasetException(("Error! Could not find a \"{0}\" file. "
+                           "Make sure that there is a \"{0}\" in the "
+                           "same directory as \"{1}.py\"! Spelling is "
+                           "very important here."
+                           ).format(_Constants._DATABASE_NAME, __name__))
+elif not _os.access(_Constants._DATABASE_NAME, _os.R_OK):
+    raise DatasetException(("Error! Could not read the \"{0}\" file. "
+                            "Make sure that it readable by changing its "
+                            "permissions. You may need to get help from "
+                            "your instructor."
+                            ).format(_Constants._DATABASE_NAME, __name__))
+
+
+_Constants._DATASET = None
+
+def get_turbines():
+    """
+    Retrieves all of the Turbines.
+    """
+    if _Constants._DATASET is None:
+        with open(_Constants._DATABASE_NAME, 'rb') as _:
+            _Constants._DATASET = _pickle.load(_)
+    return _Constants._DATASET
+
+if __name__ == '__main__':
+    from pprint import pprint as _pprint
+    from timeit import default_timer as _default_timer
+    
+    print(">>> get_turbines()")
+    
+    start_time = _default_timer()
+    result = get_turbines()
+    print("Time taken: {}".format(_default_timer() - start_time))
+    _pprint(result[0])
