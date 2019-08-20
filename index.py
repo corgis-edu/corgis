cafcdfdf24a6a62a4646b74af86d62dd505cc15e
Austin Cory Bart <acbart@vt.edu>
2019-08-20 10:30:44 -0400

    Kill Country Crime

---
+++ index.py
@@ -0,0 +1,20 @@
+import argparse
+from tools.build import build
+from tools.config import Config
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Rebuilds the index by browsing all the source datasets')
+    parser.add_argument('source', metavar='s', type=str,
+                        help='The dataset to build. Use * to build all.')
+    parser.add_argument('format', metavar='f', type=str,
+                        default='all',
+                        help='The desired target language/environment/format.')
+    parser.add_argument('--destination', metavar='d', type=str,
+                        default=Config.DEFAULT_DESTINATION,
+                        help='The destination folder to put the files.')
+
+    args = parser.parse_args()
+    configuration = Config(args.destination)
+    reports = build(args.source, args.format, configuration)
+    for report in reports:
+        print(report)
