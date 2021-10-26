3e68748bd750fb52ae73412e5518d1af3476a08c
miniymay <awdry@naver.com>
2021-10-26 00:15:45 -0400

    Update wind-turbines dataset

---
+++ source/wind-turbines/wind-turbines-script.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  1 09:04:25 2019
+
+@author: kafura
+"""
+
+import csv
+
+def has_all_data(row):
+    for item in row:
+        if item == None:
+            return(False)
+    return(True)
+    
+def cap_each(name):
+    names = name.split(' ')
+    if len(names) == 1:
+        return name.capitalize()
+    else:
+        for i in range(len(names)):
+            names[i] = names[i].capitalize()
+        return " ".join(names)  
+    
+original = open("raw/Wind-Turbine-Original.csv", "r", newline='', encoding="windows-1252")
+processed = open("wind-turbines-corgis.csv", "w", newline='\n')
+
+reader = csv.reader(original)
+writer = csv.writer(processed)
+
+header = next(reader)
+
+# columns to extract from the input file
+state = 5
+county = 6
+year = 9
+num_turbines = 10
+proj_capacity = 11     # in MW
+turbine_cap = 14       # in KW
+hub_height = 15        # in meters
+rotor_diam = 16
+swept_area = 17
+total_height = 18
+longitude = 25
+latitude = 26
+
+outrow = [None] * 12
+
+for inrow in reader:
+        
+    outrow[0] = str(inrow[state])            if inrow[state]         else None
+    outrow[1] = str(cap_each(inrow[county])) if inrow[county]        else None
+    outrow[2] = int(inrow[year])             if inrow[year]          else None
+    outrow[3] = int(inrow[turbine_cap])      if inrow[turbine_cap]   else None
+    outrow[4] = float(inrow[hub_height])     if inrow[hub_height]    else None
+    outrow[5] = float(inrow[rotor_diam])     if inrow[rotor_diam]    else None
+    outrow[6] = float(inrow[swept_area])     if inrow[swept_area]    else None
+    outrow[7] = float(inrow[total_height])   if inrow[total_height]  else None
+    outrow[8] = float(inrow[proj_capacity])  if inrow[proj_capacity] else None
+    outrow[9] = int(inrow[num_turbines])     if inrow[num_turbines]  else None
+    outrow[10] = float(inrow[longitude])     if inrow[longitude]     else None
+    outrow[11] = float(inrow[latitude])      if inrow[latitude]      else None
+
+    if has_all_data(outrow):
+        writer.writerow(outrow)
+
+original.close()
+processed.close()
+   
