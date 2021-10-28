53b60d0a94b1dc0c6682b3ef509a2e5d99f9d62e
miniymay <awdry@naver.com>
2021-10-27 23:12:35 -0400

    update opioids dataset

--- source/opioids/opioids-script.py
+++ source/opioids/opioids-script.py
@@ -7,13 +7,13 @@ Created on Thu Jun 27 13:12:03 2019
 
 import csv
 
-year_range = 2017 - 1999 + 1
+year_range = 2019 - 1999 + 1
 column_start = 2
 
 # initialize opiod data with the year field
 
 opioid_data = []
-for year in range(1999, 2018):
+for year in range(1999, 2020):
     opioid_data.append([year])
     
 # rows to extract for the number of deaths measure
@@ -21,7 +21,7 @@ for year in range(1999, 2018):
 number_rows_used = [8, 11, 14, 23, 26, 35]
 
 opioid_number_data_file_name = "./raw/Number_OD_Deaths.csv"
-opioid_number_data_file = open(opioid_number_data_file_name, 'r', newline='\n')
+opioid_number_data_file = open(opioid_number_data_file_name, 'r', newline='\n', encoding = "ISO-8859-1")
 opioid_number_reader = csv.reader(opioid_number_data_file)
 
 row_number = 1
@@ -34,14 +34,16 @@ for row in opioid_number_reader:
 # add rate and demographic (gender, race, and area) data
    
 opioid_demographic_data_file_name = "./raw/Rate_OD_by_Demographics.csv"
-opioid_demographic_data_file = open(opioid_demographic_data_file_name, 'r', newline='\n')
+opioid_demographic_data_file = open(opioid_demographic_data_file_name, 'r', newline='\n', encoding = "ISO-8859-1")
 opioid_demographic_reader = csv.reader(opioid_demographic_data_file)
 
 # need rows fro the demographic data are: 8-67 except 15-16,18-19 and 21,22
 
+deleted_row_list = [12, 13, 15, 16, 18, 19, 21, 22, 24, 25, 39, 47, 49, 55, 57, 63]
+
 row_number = 1
 for row in opioid_demographic_reader:
-    if row_number in range(8,68) and not(row_number in [15,16,18,19, 21,22]):
+    if row_number in range(8,66) and not(row_number in deleted_row_list): #15,16,18,19, 21,22
         for year in range(year_range):
             opioid_data[year].append(row[column_start + year])
     row_number = row_number + 1
